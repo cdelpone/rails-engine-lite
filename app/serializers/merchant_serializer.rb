80e4a11cd5cd45105a09ceba33e1af0404d203e0
Christina Delpone <cdelpone@gmail.com>
2021-12-08 11:32:34 -0700

    Remove relationships, add error messaging

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,5 +1,9 @@
 class MerchantSerializer
   include JSONAPI::Serializer
-  
+
   attributes :name
+
+  def self.merch_not_found
+    { errors: { message:  "This merchant id does not exist"  }  }
+  end
 end
