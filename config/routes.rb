64521a12287d7f2c60168a0a3976bf7d4cd83722
Christina Delpone <cdelpone@gmail.com>
2021-12-08 11:32:57 -0700

    Add item merch route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: :index
+      end
+      resources :items, only: [:index, :show] do
+        resources :merchants, only: :index
+      end
+      resources :items, except: :delete
+    end
+  end
 end
