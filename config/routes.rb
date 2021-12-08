7020b0947b32c5b638f5bc95cf000126efe1a455
Christina Delpone <cdelpone@gmail.com>
2021-12-08 16:40:18 -0700

    Hand roll merchants resource items route

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show] do
-        resources :items, only: :index
-      end
-      resources :items, only: [:index, :show] do
-        resources :merchants, only: :index
-      end
+      get 'merchants/:id/items', to: 'merchants_items#index'
+      # get 'items/:id/merchant', to: 'items_merchant#show'
       resources :items, except: :delete
+      resources :merchants, only: [:index, :show]
+      # resources :merchants, only: [:index, :show] do
+      #   resources :items, only: :index
+      # end
+      # resources :items, only: [:index, :show] do
+      #   resources :merchants, only: :index
+      # end
     end
   end
 end
