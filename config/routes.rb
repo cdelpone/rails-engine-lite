0522d0fe53ba1829b98dc341deaa839d967bb112
Christina Delpone <cdelpone@gmail.com>
2021-12-09 12:49:43 -0700

    Replace handrolled routes with resources, again

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get 'merchants/:id/items', to: 'merchants_items#index'
-      resources :items, except: :delete
-      resources :merchants, only: [:index, :show]
-      get 'items/:id/merchant', to: 'items_merchant#show'
+      resources :items, except: :delete do
+        resources :merchants, only: :index, controller: 'items_merchant'
+        # get 'items/:id/merchant', to: 'items_merchant#show'
+      end
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: :index, controller: 'merchants_items'
+        # get 'merchants/:id/items', to: 'merchants_items#index'
+      end
     end
   end
 end
